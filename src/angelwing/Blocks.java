41e1983347628efa3341890e94d001402383e04f
Stefan Manoil <manoilstefan302@gmail.com>
2023-03-28 18:12:53 +0300

    Am adaugat niste clase

--- src/angelwing/Blocks.java
+++ src/angelwing/Blocks.java
@@ -1,2 +1,17 @@
-package angelwing;public class Blocks {
+package angelwing;
+
+public class Blocks {
+    int x = 5;
+    static int y = 16;
+    final String name = "Stefan";
+    {
+        System.out.println("Good Job!");
+    }
+    static{
+        y = 20;
+    }
+    public Blocks (final int x){
+        this.x =x;
+    }
 }
+~
--- src/angelwing/Main.java
+++ src/angelwing/Main.java
@@ -1,2 +1,4 @@
-package angelwing;public class Main {
+package angelwing;
+
+public class Main {
 }
